1e0a23763987f9a5875702cbf378339ff9941af9
Alex Cosme <acosme1988@gmail.com>
2023-06-09 19:46:38 +0200

    2

---
+++ script/script.js
@@ -0,0 +1,22 @@
+// function enviar {
+//     let jugador1 = document.formulario.jugador1.value;
+//     if (jugador1 === "") {
+//         false; 
+//     }
+    
+// }
+// function enviar2 {
+//     let jugador2 = document.formulario.jugador2.value;
+//     jugador2 === "", false; 
+// }
+
+function click() {
+    var juego = document.getElementById("juego");
+    
+    if (juego.style.display === "none"){
+        juego.style.display = "block";
+    } 
+    else{ 
+        juego.style.display === "none";
+    }
+} 
